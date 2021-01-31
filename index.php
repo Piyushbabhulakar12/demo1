e01463faf46696602d821bd6d2c15c31d7b16ee4
PIYUSH <65109923+Piyushbabhulakar12@users.noreply.github.com>
2021-01-31 16:11:01 +0530

    index.html

--- index.php
+++ index.php
@@ -1,98 +1,424 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
- <meta charset="utf-8">
- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
     <style type="text/css">
-    	body
-    	{
-font-family: 'Poppins', sans-serif;
-    	}
-    	.bg
-    	{
-    		background-color: #fff;
-    		
-    		width: 350px;
-    	}
-    </style>
+  body
+  {
+    font-family: 'Poppins', sans-serif;
+    background: #ededed;
+  }
+  .textmarg
+  {
+    padding: 12px;
+  }
+  .date_info{
+  padding: 0;
+  margin-bottom: 74px;
+}
+.date_info li{
+  float: left;
+  list-style: none;
+  margin-right: 3%;
+  border-radius: 5px;
+  
+}
+.list
+{
+  list-style: none;
+  display: flex;
+   padding: 0px;
+  justify-content: center;
+  flex-flow: wrap;
+}
+.red
+{
+  border: 1px solid #cc3a29;
+  border-radius: 50px;
+  height: 38px;
+  width: 38px;
+  text-align: center;
+ padding-top: 7px;
+ margin: 3px;
+}
+.red:hover
+{
+  background: #cc3a29;
+  color: #fff;
+}
+.yellow
+{
+  border: 1px solid #f7b336;
+  border-radius: 50px;
+  height: 38px;
+  width: 38px;
+  text-align: center;
+ padding-top: 7px;
+ margin: 3px;
+}
+.yellow:hover
+{
+  background: #f7b336;
+  color: #fff;
+}
+.green
+{
+  border: 1px solid #71d53a;
+  border-radius: 50px;
+  height: 38px;
+  width: 38px;
+  text-align: center;
+ padding-top: 7px;
+ margin: 3px;
+}
+.green:hover
+{
+  background: #71d53a;
+  color: #fff;
+}
+.text_resp
+{
+  border: none;
+  width: 92%;
+  
+  background: #ededed;
+  border-radius: 5px;
+}
+.btn-cls
+{
+background: #2960db;
+color: #fff;
+margin-top: 20px;
+width: 150px;
+}
+.btn-cls:hover
+{
+  color: #fff;
+}
+</style>
+  </head>
+  <body>
+    
 
-	<title>Sign Up</title>
-</head>
-<body>
 
 
 
 
-  <div class="container mt-3">
-  	<div class="row">
-  		<div class="col-md-12">
-  			<center>
-  			<div class="bg shadow"><br>
-  				<i class="fa fa-user-circle-o" aria-hidden="true" style="font-size: 60px;"></i><br><br>
-  				<h3>Sign Up</h3><br>
-                 
-                 <form method="post">
-                 	   <input type="text" style="border-radius: 50px; width: 95%;" class="form-control" name="f_name" placeholder="First Name" required><br>
-                   <input type="text" style="border-radius: 50px; width: 95%;" class="form-control" name="l_name" placeholder="Last Name" required><br>
-<input type="email" style="border-radius: 50px; width: 95%;" name="email" class="form-control" placeholder="Enter Email Id" required><br>
-<input type="number" style="border-radius: 50px; width: 95%;" name="phone" class="form-control" placeholder="Enter Phone Number" required><br>
-<input type="password" style="border-radius: 50px; width: 95%;" name="pass" class="form-control" id="passw" placeholder="Password" required><br>
-<input type="password" style="border-radius: 50px; width: 95%;" name="cpass" class="form-control" id="cpassw" placeholder="Confirm Password" required><br>
-<input type="submit" style="border-radius: 50px; width: 95%;" name="submit" class="btn btn-dark" value="Sign In" onclick="myfun()"><br><br>
-            <a href="login.php"><input style="border-radius: 50px; width: 95%;" type="button" class="btn btn-dark" value="Log In" name=""><br><br>
-                 </form>
-                
-  			</div>
-  		</div>
-  	</div>
-  </div>
+
+<div class="container-fluied" style="background: #fff; height: 85px">
+  <h5 style="text-align: center; align-items: center; padding-top: 29px;">Header</h5>
+</div>
 
 
 
 
-<?php
-
- $conn = mysqli_connect("localhost","root","","learn");
-
- if (isset($_POST['submit'])) 
- {
- 
-      $f_name = $_POST['f_name'];
-      $l_name = $_POST['l_name'];
-      $email = $_POST['email'];
-      $phone = $_POST['phone'];
-      $pass = $_POST['pass'];
-
-      $pass = md5($pass);
-
-      $sql = " insert into sign (f_name,l_name,email,phone,pass) values ('$f_name','$l_name','$email','$phone','$pass') ";
-
-      if (mysqli_query($conn,$sql)) 
-      {
-      	
+<div class="container mt-5" >
+  <div class="row">
+    
+       <div class="col-md-6 align-self-top mb-4">
         
+              <div style="background: #fff; border-radius: 10px;  border: none; ">
+                
+                
+              
+                <div class="float-start textmarg">Start Work<br>
 
-      }
-      else
-      {
-      	echo "Not insertd";
-      }
+                  
+                  <h5>Wednesday <span style="font-size: 12px;">(On Time)</span></h5>
+                  <p>February</p>
+
+                  <h1 style="color: #2960db; font-size: 80px;">03<span style="font-size: 14px; color: #333333;">2021</span></h1>
+
+               Status:
+
+               <h2>IN TRANSIT</h1>
+
+               
 
 
- }
-
-
-?>
 
 
 
 
- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
-</body>
+
+                </div><br>
+             
+
+
+
+
+
+
+                <div class="float-end textmarg">Date</div>
+  <center><div style="clear: both; border-top: 1px solid #d3d3d3; padding-top: 2px; width: 93%;"></div></center>
+
+               
+              
+
+              <div class="float-start textmarg">
+                
+
+
+              Delhivery 
+ 
+              </div>
+
+
+              <div class="float-end textmarg">
+             
+                      <span style="font-size: 13px;">Tracking ID</span><br>
+                      <span style="color: #2960db; font-size: 14px;">109191294253</span>
+              </div>
+
+
+      <br><br>
+
+            <div class="textmarg mt-4">
+              
+              
+              <div class="row mt-4">
+                <div class="col-3">
+                  <p style="text-align: right; font-size: 14px;">
+                    30 JAN<br>
+                  06:44 PM
+                  </p>
+                </div>
+                <div class="col-2">
+                  
+                </div>
+                <div class="col-7">
+                  <p style="font-size: 14px;">Activity : In Transit - Added to Bag
+Location : Patna_Gaighat (Bihar)</p>
+                </div>
+              </div>
+
+
+              <div class="row mt-4">
+                <div class="col-3">
+                  <p style="text-align: right; font-size: 14px;">
+                    30 JAN<br>
+                  06:44 PM
+                  </p>
+                </div>
+                <div class="col-2">
+                  
+                </div>
+                <div class="col-7">
+                  <p style="font-size: 14px;">Activity : In Transit - Added to Bag
+Location : Patna_Gaighat (Bihar)</p>
+                </div>
+              </div>
+
+               <div class="row mt-4">
+                <div class="col-3">
+                  <p style="text-align: right; font-size: 14px;">
+                    30 JAN<br>
+                  06:44 PM
+                  </p>
+                </div>
+                <div class="col-2">
+                  
+                </div>
+                <div class="col-7">
+                  <p style="font-size: 14px;">Activity : In Transit - Added to Bag
+Location : Patna_Gaighat (Bihar)</p>
+                </div>
+              </div>
+
+
+               <div class="row mt-4">
+                <div class="col-3">
+                  <p style="text-align: right; font-size: 14px;">
+                    30 JAN<br>
+                  06:44 PM
+                  </p>
+                </div>
+                <div class="col-2">
+                  
+                </div>
+                <div class="col-7">
+                  <p style="font-size: 14px;">Activity : In Transit - Added to Bag
+Location : Patna_Gaighat (Bihar)</p>
+                </div>
+              </div>
+
+
+
+ 
+
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+              </div>
+
+       </div>
+
+       <div class="col-md-6 align-self-top mb-4">
+        
+            
+            <div class="textmarg" style="background: #fff; border-radius: 10px; height: 150px; border: none;">
+
+              <p style="padding-top: 8px;">Order Details</p>
+
+
+            <center><div style="clear: both; border-top: 1px solid #d3d3d3; padding-top: 2px; width: 95%;"></div></center>
+
+
+
+            <div style="padding-top: 11px;">
+                <span class="float-start" style=" font-size: 13px;"><b>Order ID</b> </span><span class="float-end" style="color: #737373; width: 65%;
+  word-break: break-word; font-size: 13px;">406-1977721-971072</span>
+            </div>
+            <br>
+
+            <div style="padding-top: 10px;">
+                <span class="float-start" style=" font-size: 13px;"><b>Order Placed On</b> </span><span class="float-end" style="color: #737373; width: 65%;
+  word-break: break-word; font-size: 13px;">29th Jan 2021</span>
+            </div>
+           
+   
+
+
+
+
+
+
+
+
+              
+            </div>
+
+              <div class="textmarg" style="background: #fff; border-radius: 10px; border: none; margin-top: 27px; ">
+
+             <p style="font-size: 14px; text-align: center; padding-top: 10px;">Based on your recent interaction with SARFUDDIN FLUTES, how likely are you to recommend SARFUDDIN FLUTES to friends & family?</p>
+
+
+             <ul class="list">
+                  <li class="red">0</li>
+                  <li class="red">1</li>
+                  <li class="red">2</li>
+                  <li class="red">3</li>
+                  <li class="yellow">4</li>
+                  <li class="yellow">5</li>
+                  <li class="yellow">6</li>
+                  <li class="yellow">7</li>
+                  <li class="green">8</li>
+                  <li class="green">9</li>
+                  <li class="green">10</li>
+             </ul>
+                   
+
+
+             <div class="float-start" style="padding-left: 32px;">
+               <i class="fas fa-chevron-down"></i><br>
+                
+             </div>
+
+             <div class="float-end" style="padding-right: 32px;">
+               <i class="fas fa-chevron-down" style="text-align: left;"></i>
+             </div>
+                
+<br>
+                <p style="text-align: center;">Remark</p>
+
+
+                  <center>
+                    <input type="text" name="" placeholder="Enter Your Remark" class="text_resp form-control" >
+                    <input type="button" name="" value="Submit" class="btn btn-cls">
+                  </center>
+
+<p></p>
+
+
+
+                
+              </div>
+
+                  
+                 <div class="row">
+                  <div class="col-xl-6 col-sm-12">
+                    
+                    <div class="textmarg" style="background: #fff; border-radius: 10px; border: none; margin-top: 27px; ">
+
+                      <p>Order Detail</p>
+
+                      <p style="font-size: 13px;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                
+                        </div>
+
+                  </div>
+
+                  <div class="col-xl-6 col-sm-12">
+
+                    <div class="textmarg" style="background: #fff; border-radius: 10px; border: none; margin-top: 27px; ">
+
+                      <p>Order Detail</p>
+
+                      <p style="font-size: 13px;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                
+                         </div>
+
+                  </div>
+
+                 </div>
+ 
+
+
+       </div>
+
+
+  </div>
+</div>
+
+
+
+
+<div class="container-fluied " style="background: #fff; height: 85px">
+  <h5 style="text-align: center; align-items: center; padding-top: 29px;">Footer</h5>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </body>
 </html>
